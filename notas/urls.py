5d05c8e428f0a0d676d71570e2a5628bbc043858
Fernando Guerrero <devrrior@gmail.com>
2022-02-01 00:35:58 -0600

    add(views): add CRUD

--- notas/urls.py
+++ notas/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from notas.views import HomePageView
+from notas.views import HomePageView, CreateNotaView, UpdateNotaView, DeleteNotaView
 
 urlpatterns = [
 	path('', HomePageView.as_view(), name='home'),
+	path('new/', CreateNotaView.as_view(), name='new'),
+	path('edit/<int:pk>', UpdateNotaView.as_view(), name='edit'),
+	path('delete/<int:pk>', DeleteNotaView.as_view(), name='delete'),
 ]
--- notas/views.py
+++ notas/views.py
@@ -1,4 +1,35 @@
-from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-class HomePageView(TemplateView):
-  template_name = "notas/home.html"
+from notas.models import Nota
+
+# GET
+# Muestre mis notas
+class HomePageView(ListView):
+  # template_name = 'notas/home.html'
+  model = Nota
+
+# POST
+# Cree notas
+class CreateNotaView(CreateView):
+  # template_name = 'notas/nuevo.html'
+  model = Nota
+  fields = ['titulo', 'descripcion']
+  success_url = reverse_lazy('home')
+
+
+# PUT / PATCH
+# Actualizar notas
+class UpdateNotaView(UpdateView):
+  # template_name = 'notas/actualizar.html'
+  model = Nota
+  fields = ['titulo', 'descripcion']
+  success_url = reverse_lazy('home')
+
+# DELETE
+# Eliminar Notas
+class DeleteNotaView(DeleteView):
+  # template_name = 'notas/eliminar.html'
+  model = Nota
+  success_url = reverse_lazy('home')
