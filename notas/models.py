4ef6ecdee6336aab7b65abdb5083af5d9384bb51
Fernando Guerrero <devrrior@gmail.com>
2022-02-01 00:35:08 -0600

    add(models): create new table

--- notas/models.py
+++ notas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Nota(models.Model):
+	titulo = models.CharField(max_length=255)
+	descripcion = models.CharField(max_length=255)
+	fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f'Titulo: {self.titulo} - Descripcion: {self.descripcion}'
